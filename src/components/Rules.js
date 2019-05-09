5325c1f4dacf219c8eef0e31c6c00801ee117ed6
lauri koti <lauriaus@metropoli.fi>
2019-05-09 22:58:54 +0300

    'ddgd'

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -8,77 +8,84 @@ import Modal from 'react-bootstrap/Modal';
 
 class Rules extends React.Component {
 
-  constructor(props, context) {
-    super(props, context)
-      this.handleClick = this.handleClick.bind(this)
-    this.handleShow = this.handleShow.bind(this);
-    this.handleClose = this.handleClose.bind(this);
-      this.state = { visible: false }
-    this.state = {
-      roomName: '',
-      show: false
-    }
+    constructor(props, context) {
+        super(props, context)
+        this.handleClick = this.handleClick.bind(this)
+        this.handleShow = this.handleShow.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.state = { visible: false }
+        this.state = {
+            roomName: '',
+            show: false
+        }
 
-  }
+    }
     handleClick() {
         this.setState(prev => ({ visible: !prev.visible }))
     }
-  handleClose() {
-    this.setState({ show: false });
-  }
+    handleClose() {
+        this.setState({ show: false });
+    }
 
-  handleShow() {
-    this.setState({ show: true });
-  }
+    handleShow() {
+        this.setState({ show: true });
+    }
 
-  render () {
-    return (
-        <>
-          <Button className="tausta" variant="primary" onClick={this.handleShow}>
-            Ohjeet
-          </Button>
+    render () {
+        return (
+            <>
+                <Button className="tausta" variant="primary" onClick={this.handleShow}>
+                    Ohjeet
+                </Button>
 
-          <Modal className="tyhja" show={this.state.show} onHide={this.handleClose} >
-            <Modal.Header closeButton>
-              <Modal.Title>   <h1> Ohjeet tarkoittavat sivustolla kiellettyä sisältöä.  <br></br></h1>
-              </Modal.Title>
-            </Modal.Header>
-            <Modal.Body className="row">
+                <Modal className="tyhja" show={this.state.show} onHide={this.handleClose} >
+                    <Modal.Header closeButton>
+                        <Modal.Title>   <h1> Ohjeet tarkoittavat sivustolla kiellettyä sisältöä.  <br></br></h1>
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body className="row">
 
-                <br></br>   <br></br>
-                <h2>  Laiton materiaali</h2>
-                <br></br>
-                Tähän sisältyy lapsiporno ja lasten seksualisointi (myös selvästi lasta esittävät piirroshahmot jne.), eläinporno, tosielämässä tapahtuva väkivalta, eläinrääkkäys, laiton kaupankäynti ja muu laiton materiaali.
-                Asiattomuudet tai muiden keskustelijoiden haukkuminen
-                Tämä sisältää myös runsaan kiroilun.
-                <br></br> <br></br>
-                <h2>  Roskapostitus</h2>
-                <br></br>
-                Roskapostitus tarkoittaa toistuvaa esimerkiksi sisällöttömien, samansisältöisten tai aiheeseen liittymättömien viestien tai kysymysten lähettämistä.
-                <br></br> <br></br>
-                    <h2>  Mainostaminen</h2>
-                <br></br>
-                Et saa mainostaa Fuksifoorumin keskusteluissa ilman ylläpidon kirjallista suostumusta. Tämä sisältää myös referrer-linkit, joiden klikkaamisesta hyötyy joku muu henkilö, kuin klikkaaja.
-                Haitalliset tiedostot tai linkit
-                Kaikkien haitallisten tiedostojen ja linkkien lähettäminen on kielletty. Tähän sisältyy esimerkiksi haittaohjelmat, virukset, selauslaitteelle haittaa aiheuttavat tiedostot tai -sivustot tai linkittäminen näihin. Tähän sisältyy myös linkinlyhentimet.
-                Alastomuus tai pornografia
-                Alastomuudeksi lasketaan esimerkiksi intiimialueiden näkyminen ja lähikuvat niukasti peitetyistä intiimialueista.
-                <br></br> <br></br>
-                <h2>  Muuta</h2>
-                <br></br>
-                Moderaattorin esittäminen sivustolla on ehdottomasti kiellettyä.
-                Lähettämällä tiedoston Fuksifoorumille, luovutat sen vapaaseen ei-kaupalliseen jakeluun. Kaupallisen käytön tekijänoikeudet säilyvät yhä sinulla.
-                Sääntöjä voidaan ilman erillistä ilmoitusta lisätä, poistaa tai muuttaa.
-                Ilmoitamme kaikista ilmoitusvelvollisuuden alaisista lakirikkeistä viranomaisille.
-                <br></br> <br></br>
-                Hyväksyt nämä ehdot käyttämällä tätä sivustoa.
+                        <br></br>   <br></br>
+                        <h2>  Laiton materiaali</h2>
+                        <br></br>
+                        Tähän sisältyy eläinporno, tosielämässä tapahtuva väkivalta, eläinrääkkäys, laiton kaupankäynti ja muu laiton materiaali.
+                        <br></br><br></br>
+                        <h2>Asiattomuudet tai muiden keskustelijoiden haukkuminen</h2>
+                        <br></br>
+                        Tämä sisältää myös runsaan kiroilun.
+                        <br></br> <br></br>
+                        <h2>Roskapostitus</h2>
+                        <br></br>
+                        Roskapostitus tarkoittaa toistuvaa esimerkiksi sisällöttömien, samansisältöisten tai aiheeseen liittymättömien viestien tai kysymysten lähettämistä.
+                        <br></br> <br></br>
+                        <h2>  Mainostaminen</h2>
+                        <br></br>
+                        Et saa mainostaa Fuksifoorumin keskusteluissa ilman ylläpidon kirjallista suostumusta. Tämä sisältää myös referrer-linkit, joiden klikkaamisesta hyötyy joku muu henkilö, kuin klikkaaja.
+                        <br></br><br></br>
+                        <h2>Haitalliset tiedostot tai linkit</h2>
+                        <br></br>
+                        Kaikkien haitallisten tiedostojen ja linkkien lähettäminen on kielletty. Tähän sisältyy esimerkiksi haittaohjelmat, virukset, selauslaitteelle haittaa aiheuttavat tiedostot tai -sivustot tai linkittäminen näihin. Tähän sisältyy myös linkinlyhentimet.
+                        Alastomuus tai pornografia
+                        Alastomuudeksi lasketaan esimerkiksi intiimialueiden näkyminen ja lähikuvat niukasti peitetyistä intiimialueista.
+                        <br></br> <br></br>
+                        <h2>  Muuta</h2>
+                        <br></br>
+                        Moderaattorin esittäminen sivustolla on ehdottomasti kiellettyä.
+                        <br></br>
+                        Lähettämällä tiedoston Fuksifoorumille, luovutat sen vapaaseen ei-kaupalliseen jakeluun. Kaupallisen käytön tekijänoikeudet säilyvät yhä sinulla.
+                        <br></br>
+                        Sääntöjä voidaan ilman erillistä ilmoitusta lisätä, poistaa tai muuttaa.
+                        <br></br>
+                        Ilmoitamme kaikista ilmoitusvelvollisuuden alaisista lakirikkeistä viranomaisille.
+                        <br></br> <br></br>
+                        <h4>Hyväksyt nämä ehdot käyttämällä tätä sivustoa.</h4>
 
-            </Modal.Body>
+                    </Modal.Body>
 
-          </Modal>
-        </>
-    )
-  }
+                </Modal>
+            </>
+        )
+    }
 }
 
 

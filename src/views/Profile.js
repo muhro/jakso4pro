1bce092213264f2c92057179d2a5b645ee104e5d
lauri koti <lauriaus@metropoli.fi>
2019-05-09 20:36:53 +0300

    'ddgd'

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -4,63 +4,66 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import {Redirect} from 'react-router-dom';
 import {
-  Button,
-  Card,
-  CardActionArea,
-  CardContent,
-  CardMedia,
-  Typography,
+    Button,
+    Card,
+    CardActionArea,
+    CardContent,
+    CardMedia,
+    Typography,
 } from '@material-ui/core';
 
 const mediaUrl = 'http://media.mw.metropolia.fi/wbma/uploads/';
 
 const styles = {
-  card: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 300,
-    objectFit: 'cover'
-  },
+    card: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 300,
+        objectFit: 'cover'
+    },
 };
 
 const Profile = (props) => {
-  if (props.user === null) {
-    return <Redirect to="/" />;
-  }
+    if (props.user === null) {
+        return <Redirect to="/" />;
+    }
 
-  /*const {username, email, full_name, profilePic} = props.user;
-  const { classes } = props;*/
-  return (
-      <React.Fragment>
-        <h1>Profile</h1>
-        {/*<Card className={classes.card}>
-          <CardActionArea>
-            <CardMedia className={classes.media}
+    const {username, email, full_name, profilePic} = props.user;
+    const { classes } = props;
+    return (
+        <React.Fragment>
+            <h1>Profile</h1>
+            <Card className={classes.card}>
+                <CardActionArea>
+                    {/*<CardMedia className={classes.media}
                        image={mediaUrl + profilePic.filename} title={username}/>
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                {username}
-              </Typography>
-              <Typography component="p">
-                email: {email}
-              </Typography>
-              <Typography component="p">
-                Full name: {full_name}
-              </Typography>
-              <Button component={Link} to="/my-files">
-                My Files
-              </Button>
-            </CardContent>
-          </CardActionArea>
-        </Card>*/}
-      </React.Fragment>
-  );
+            <CardContent>*/}
+                    <CardMedia className={classes.media}
+                               image={mediaUrl + profilePic} title={username}/>
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            {username}
+                        </Typography>
+                        <Typography component="p">
+                            email: {email}
+                        </Typography>
+                        <Typography component="p">
+                            Full name: {full_name}
+                        </Typography>
+                        <Button component={Link} to="/my-files">
+                            My Files
+                        </Button>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </React.Fragment>
+    );
 };
 
 Profile.propTypes = {
-  user: PropTypes.object,
-  classes: PropTypes.object.isRequired,
+    user: PropTypes.object,
+    classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(Profile);
